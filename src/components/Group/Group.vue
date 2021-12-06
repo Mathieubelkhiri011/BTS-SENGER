c3996d328041d494d8d7f4e4160f705ddef9ed6b
Mathieubelkhiri011 <74365924+Mathieubelkhiri011@users.noreply.github.com>
2021-12-06 17:43:18 +0100

    création liste Groupe

--- src/components/Group/Group.vue
+++ src/components/Group/Group.vue
@@ -2,7 +2,7 @@
   <div class="group">
     <div class="ui fluid search">
       <div class="ui icon input">
-        <input
+        <input v-model="search"
           type="text"
           placeholder="Rechercher un utilisateur"
           class="prompt"
@@ -14,72 +14,25 @@
       <span>Participants</span>
       <hr />
     </div>
-    <div class="user">
-      <img src="https://source.unsplash.com/mK_sjD0FrXw/100x100" /><span
-        >Alice<br /><i class="nickname"></i></span
-      ><i title="Modifier le surnom" class="circular quote left link icon"></i
-      ><i
-        title="Enlever de la conversation"
-        class="circular times icon link"
-        style=""
-      ></i>
-    </div>
-    <div class="user">
-      <img src="https://source.unsplash.com/7omHUGhhmZ0/100x100" /><span
-        >Bob<br /><i class="nickname"></i></span
-      ><i title="Modifier le surnom" class="circular quote left link icon"></i
-      ><i
-        title="Enlever de la conversation"
-        class="circular times icon link"
-        style=""
-      ></i>
-    </div>
-    <div class="user">
-      <img src="https://source.unsplash.com/FUcupae92P4/100x100" /><span
-        >Derek<br /><i class="nickname"></i></span
-      ><i title="Modifier le surnom" class="circular quote left link icon"></i
-      ><i
-        title="Enlever de la conversation"
-        class="circular times icon link"
-      ></i>
-    </div>
-    <div class="user">
-      <img src="https://source.unsplash.com/OYH7rc2a3LA/100x100" /><span
-        >Gael<br /><i class="nickname"></i></span
-      ><i title="Modifier le surnom" class="circular quote left link icon"></i
-      ><i
-        title="Enlever de la conversation"
-        class="circular times icon link"
-        style=""
-      ></i>
+    <div class="user" v-for="user in UsersConversations" :key="user.username">
+      <img v-bind:src=user.picture_url />
+      <span> {{ user.username }} <br /><i class="nickname"></i></span>
+      <i title="Modifier le surnom" class="circular quote left link icon"></i>
+      <i title="Enlever de la conversation" class="circular times icon link" style=""></i>
     </div>
     <div class="spanner">
       <hr />
       <span>Communauté</span>
       <hr />
     </div>
-    <div class="user">
-      <img src="https://source.unsplash.com/8wbxjJBrl3k/100x100" /><span
-        >Cha</span
-      ><i title="Ajouter à la conversation" class="circular plus icon link"></i>
-    </div>
-    <div class="user">
-      <img src="https://source.unsplash.com/4U1x6459Q-s/100x100" /><span
-        >Emilio</span
-      ><i title="Ajouter à la conversation" class="circular plus icon link"></i>
-    </div>
-    <div class="user">
-      <img src="https://source.unsplash.com/3402kvtHhOo/100x100" /><span
-        >Fabrice</span
-      ><i title="Ajouter à la conversation" class="circular plus icon link"></i>
-    </div>
-    <div class="user">
-      <img src="https://source.unsplash.com/tNCH0sKSZbA/100x100" /><span
-        >Benji</span
-      ><i title="Ajouter à la conversation" class="circular plus icon link"></i>
+    <div class="user" v-for="user in FilterUsers" :key="user.token">
+      <img v-bind:src=user.picture_url />
+      <span> {{ user.username }} </span>
+      <i title="Ajouter à la conversation" @click="print()" class="circular plus icon link"></i>
     </div>
   </div>
 </template>
+
 
 <script>
 import { mapGetters, mapActions } from "vuex";
@@ -88,14 +41,43 @@ export default {
   name: "Group",
   data() {
     return {
-      search: ""
+      search: "",
+      UsersConversations : []
     };
   },
   computed: {
-    ...mapGetters([])
+    ...mapGetters(["users", "conversation"]),
+
+    FilterUsers() {
+      let FilterUsers =[];
+      FilterUsers = this.users.map(user => ({
+        ...user
+      }));
+      return  FilterUsers.filter((user) => user.username.toLowerCase().includes(this.search.toLowerCase()))
+    },
   },
   methods: {
-    ...mapActions([])
+    ...mapActions([]),
+
+    AllUsersConversations() {
+      this.UsersConversations = this.users.map(user => ({
+        ...user
+      }));
+      console.log(this.UsersConversations);
+      return this.UsersConversations.filter((user) => this.conversation[0].participants.find(element => element === user.username));
+    },
+
+
+    print(){
+      this.UsersConversations = this.users.map(user => ({
+        ...user
+      }));
+      this.UserConversation = this.UsersConversations.filter((user) => this.conversation[0].participants.find(element => element === user.username));
+      console.log("tessssst", this.UserConversation[1].username);
+    }
+  },
+  mounted(){
+      this.UsersConversations = this.users.filter((user) => this.conversation[0].participants.find(element => element === user.username));
   }
 };
 </script>
